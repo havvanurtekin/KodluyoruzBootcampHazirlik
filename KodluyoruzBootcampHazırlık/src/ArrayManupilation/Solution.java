47ecba53097f687890beda0151aa590381f20a3d
Havva Nur TEKİN <20120205304@ismu.edu.tr>
2021-12-17 04:18:50 +0300

    Print in Reverse ve Array Manupilation

---
+++ KodluyoruzBootcampHazırlık/src/ArrayManupilation/Solution.java
@@ -0,0 +1,108 @@
+package ArrayManupilation;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'arrayManipulation' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static long arrayManipulation(int n, List<List<Integer>> queries) {
+    // Write your code here
+    
+    long max = 0;
+    long dizi[] = new long[n+1];
+    //List<Long> yeni = Arrays.stream(dizi)
+    //                               .boxed()
+    //                              .collect(Collectors.toCollection(ArrayList::new));
+    Integer[][] array = queries.stream()
+    .map(l -> l.stream().toArray(Integer[]::new))
+    .toArray(Integer[][]::new);
+    int i = 0;
+    int l = array.length;
+    while(l-- > 0){
+        int a = array[i][0];
+        int b = array[i][1];
+        int k = array[i][2];
+        dizi[a-1] += k;
+        dizi[b] -= k;
+        i++;
+    }
+    long toplam = 0;
+    for(int j = 0; j < dizi.length ; j++){
+        toplam += dizi[j];
+        max = Math.max(max,toplam);
+    }
+    /*
+    int j = 0,z = 0;
+    int a = j;
+    int b = j+1;
+    int k = j+2;
+    for(int i = 0; i < queries.size(); i++){
+          
+        for(int c=queries.get(i).get(a)-1; c <= queries.get(i).get(b)-1; c++){
+            long temp = yeni.get(c);
+            temp += queries.get(i).get(k);
+            yeni.set(c,temp); 
+            
+        }
+   
+    }
+    
+    
+    max = Collections.max(yeni);*/
+    return max;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<List<Integer>> queries = new ArrayList<>();
+
+        IntStream.range(0, m).forEach(i -> {
+            try {
+                queries.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        long result = Result.arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
